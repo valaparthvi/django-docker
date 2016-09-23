930783ed8a310e4520cae9a91925a54a2b773619
Vatsal Parekh <vatsalparekh@outlook.com>
2016-09-23 19:19:43 +0530

    Merged the args parse to main file

--- cli_arg.py
+++ cli_arg.py
@@ -3,16 +3,14 @@ import argparse
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--image", "-i", help="Your docker image name", type=str)
-
-parser.add_argument("--name", "-n", help="enter name", type=str)
-
+parser.add_argument("--name", "-n", help="Name of the Container", type=str)
 parser.add_argument("--entrypoint", "-e",
-                    help="Your Entrypoint File", type=str)
-
+                    help="Your Entrypoint File '.sh'", type=str)
 parser.add_argument("--working_dir", "-w", help="Working Directory", type=str)
-
 parser.add_argument("--volumes", "-v", help="Volume list", type=list)
 
 args = parser.parse_args()
+
+print args.image
 
 print(args.image, args.name, args.entrypoint, args.working_dir, args.volumes)
--- docker-py.py
+++ docker-py.py
@@ -1,12 +1,38 @@
 import os
+import argparse
 from docker import Client
 
-# base_url is where the docker daemon is running
-# currently for the same host system
+
+# Parser to get args input from CLI
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--image", "-i", help="Your docker image name", type=str)
+parser.add_argument("--name", "-n", help="Name of the Container", type=str)
+parser.add_argument("--entrypoint", "-e",
+                    help="Your Entrypoint File '.sh'", type=str)
+parser.add_argument("--working_dir", "-w", help="Working Directory", type=str)
+parser.add_argument("--volumes", "-v", help="Volume list", type=list)
+
+args = parser.parse_args()
+
+print (args.image)
+print(args.image, args.name, args.entrypoint, args.working_dir, args.volumes)
+
+
+# Cli is a Docker Client
+# base_url is where the Docker daemon is running
+# for the Docker running on the same host, use the default
+
 cli = Client(base_url='unix://var/run/docker.sock')
 
-# project wording directory
-wd = os.getcwd()
+
+# Working Directory where the codebase is
+
+if args.working_dir is None:
+    wd = os.getcwd()
+else:
+    wd = args.working_dir
 
 
 def build():
@@ -23,8 +49,11 @@ def run(info):
         working_dir=wd,
         volumes=['']
     )
-    print container
+
+    print (container)
+
     response = cli.start(container=container.get('Id'))
+    print (response)
 
 
 if __name__ == '__main__':
