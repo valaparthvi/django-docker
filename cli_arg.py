8998f263ae9506013bdb5035b96f082cfa7ce7c0
Parthvi Vala <parthvi.vala@gmail.com>
2016-09-23 18:12:41 +0530

    Command line argument

---
+++ cli_arg.py
@@ -0,0 +1,18 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--image", "-img", help="Your docker image name", type=str)
+
+parser.add_argument("--name", "-n", help="enter name", type=str)
+
+parser.add_argument("--entrypoint", "-e",
+                    help="Your Entrypoint File", type=str)
+
+parser.add_argument("--working_dir", "-w", help="Working Directory", type=str)
+
+parser.add_argument("--volumes", "-v", help="Volume list", type=list)
+
+args = parser.parse_args()
+
+print(args.image, args.name, args.entrypoint, args.working_dir, args.volumes)
