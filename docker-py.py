92760f4cc126bf32825be998cadd4d80ce3b0cd1
Vatsal Parekh <vatsalparekh@outlook.com>
2016-09-25 14:02:44 +0530

    Added more args

--- docker-py.py
+++ docker-py.py
@@ -13,6 +13,17 @@ parser.add_argument("--entrypoint", "-e",
                     help="Your Entrypoint File '.sh'", type=str)
 parser.add_argument("--working_dir", "-w", help="Working Directory", type=str)
 parser.add_argument("--volumes", "-v", help="Volume list", type=list)
+parser.add_argument(
+    "--docker_host", help='''tcp://ip-addr for the host where your docker daemon is running,
+    other then host of your local system''')
+parser.add_argument("--path", "-p",
+                    help='''Path to the directory,
+                    where your Dockerfile is located''')
+parser.add_argument("--tag", "-t", help="Tag for the image")
+parser.add_argument("--dockerfile", "-df", help="Dockerfile")
+parser.add_argument("--container_limits", "-c",
+                    help='''JSON for container resources:
+                    memory, memswap, cpushares, cpusetcpus''')
 
 args = parser.parse_args()
 
@@ -30,31 +41,31 @@ cli = Client(base_url='unix://var/run/docker.sock')
 # Working Directory where the codebase is
 
 if args.working_dir is None:
-    args.working_dir = os.getcwd()
+    wd = os.getcwd()
+else:
+    wd = args.working_dir
 
 
 def build():
     pass
 
 
-def run(args):
+def run(info):
 
-    # Create container from the given arg inputs
     container = cli.create_container(
-        image=args.image,
-        # command=args.command,
-        name=args.name,
-        entrypoint=args.entrypoint,
-        working_dir=args.working_dir,
-        volumes=args.volumes
+        image='django',
+        command='/bin/sleep 1000',
+        name='Demo-Django-Docker',
+        entrypoint=[''],
+        working_dir=wd,
+        volumes=['']
     )
+
     print (container)
 
-    container_id = container['id']
-
-    response = cli.start(container=container.get(container_id))
+    response = cli.start(container=container.get('Id'))
     print (response)
 
 
 if __name__ == '__main__':
-    run(args)
+    run()
