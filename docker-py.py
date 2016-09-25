d3d931486b1b1286f120ebd5553628555517ea97
Vatsal Parekh <vatsalparekh@outlook.com>
2016-09-25 15:38:15 +0530

    Changes:

--- docker-py.py
+++ docker-py.py
@@ -4,17 +4,18 @@ from docker import Client
 
 
 # Parser to get args input from CLI
-
 parser = argparse.ArgumentParser()
 
+# args flaps to get inputs
+parser.add_argument("--mode", "-m", help="Mode for Docker build/run")
 parser.add_argument("--image", "-i", help="Your docker image name", type=str)
 parser.add_argument("--name", "-n", help="Name of the Container", type=str)
 parser.add_argument("--entrypoint", "-e",
                     help="Your Entrypoint File '.sh'", type=str)
 parser.add_argument("--working_dir", "-w", help="Working Directory", type=str)
 parser.add_argument("--volumes", "-v", help="Volume list", type=list)
-parser.add_argument(
-    "--docker_host", help='''tcp://ip-addr for the host where your docker daemon is running,
+parser.add_argument("--docker_host",
+                    help='''tcp://ip-addr for the host where your docker daemon is running,
     other then host of your local system''')
 parser.add_argument("--path", "-p",
                     help='''Path to the directory,
@@ -34,23 +35,26 @@ print(args.image, args.name, args.entrypoint, args.working_dir, args.volumes)
 # Cli is a Docker Client
 # base_url is where the Docker daemon is running
 # for the Docker running on the same host, use the default
-
-cli = Client(base_url='unix://var/run/docker.sock')
+if args.docker_host is not None:
+    cli = args.docker_host
+else:
+    cli = Client(base_url='unix://var/run/docker.sock')
 
 
 # Working Directory where the codebase is
-
 if args.working_dir is None:
     wd = os.getcwd()
 else:
     wd = args.working_dir
 
 
+# Build function for Docker Build
 def build():
     pass
 
 
-def run(info):
+# Run function for Docker Run
+def run(args):
 
     container = cli.create_container(
         image='django',
@@ -62,10 +66,17 @@ def run(info):
     )
 
     print (container)
-
     response = cli.start(container=container.get('Id'))
-    print (response)
 
 
 if __name__ == '__main__':
-    run()
+
+    if args.mode == 'run' or 'r':
+        run(args)
+
+    if args.mode == 'build' or 'b':
+        build(args)
+
+    if args.mode == 'build+run' or 'b+r':
+        build(args)
+        run(args)
